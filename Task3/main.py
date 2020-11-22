6aeebffdb0bc5af373f31fd6ca61cc23b44f3201
Darya Belyavskaya <darya.belyavskaya.05@mail.ru>
2020-11-22 15:28:10 +0300

    add comments

--- Task3/main.py
+++ Task3/main.py
@@ -8,7 +8,12 @@ def function(arg):
     global a
     lock.acquire()
     for _ in range(arg):
+        # we can call lock.acquire() and lock.release() inside the loop, but calling this functions
+        # is much more consuming than incrementing a variable, so in this specific situation much suitable to cover
+        # in mutex whole loop.
+        # lock.acquire()
         a += 1
+        # lock.release()
     lock.release()
 
 
